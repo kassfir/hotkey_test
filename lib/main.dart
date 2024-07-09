d49c4335d45c790789a00bf6ffa7a13557465b4a
Kaspars Eglitis <kaspars@neuraldsp.com>
2024-07-09 17:41:54 +0300

    Changed the way focus is handled

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hotkey_test/custom_controller.dart';
@@ -44,9 +46,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
       body: Center(
           child: KeyboardShortcutTest(
         intent: const SnackbarIntent(),
@@ -54,24 +53,35 @@ class _MyHomePageState extends State<MyHomePage> {
         hotkey: LogicalKeyboardKey.escape,
         child: Focus(
           autofocus: true,
+          onFocusChange: (isFocused) {
+            final message = isFocused
+                ? "Parent widget gained focus."
+                : "Parent widget lost focus.";
+
+            print('----> $message');
+          },
           child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
               const Text(
                   "Press Esc to trigger a snackbar or press the button to open an overlay"),
               OverlayPortal(
                 controller: controller,
                 overlayChildBuilder: (context) {
+                  final FocusNode focusNode = FocusNode();
+
                   return KeyboardShortcutTest(
                     intent: const OverlaySnackbarIntent(),
                     snackbarMessage: "Overlay widget",
                     hotkey: LogicalKeyboardKey.space,
                     child: Focus(
-                      autofocus: true,
                       focusNode: overlayFocusNode,
                       child: Center(
                         child: TapRegion(
                           consumeOutsideTaps: true,
                           onTapOutside: (event) {
+                            inspect(FocusScope.of(context));
+                            focusNode.dispose();
                             controller.hide();
                             setState(() {});
                           },
@@ -81,12 +91,43 @@ class _MyHomePageState extends State<MyHomePage> {
                             height: 400,
                             padding: const EdgeInsets.all(20),
                             alignment: Alignment.center,
-                            child: const Text(
-                              'Notice that the snackbar message changes, but both SnackbarIntents produce the same message when Esc of Space is pressed. Click outside the overlay to close it.',
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
-                              textAlign: TextAlign.center,
+                            child: Column(
+                              children: [
+                                const Text(
+                                  'Notice that the snackbar message changes, but both SnackbarIntents produce the same message when Esc of Space is pressed. Click outside the overlay to close it.',
+                                  style: TextStyle(color: Colors.white),
+                                  textAlign: TextAlign.center,
+                                ),
+                                const SizedBox(height: 12),
+                                const Text(
+                                  'With FocusNode',
+                                  style: TextStyle(color: Colors.white),
+                                  textAlign: TextAlign.center,
+                                ),
+                                TextField(
+                                  autofocus: false,
+                                  focusNode: focusNode,
+                                  onTapOutside: (event) {
+                                    focusNode.unfocus(
+                                        disposition: UnfocusDisposition
+                                            .previouslyFocusedChild);
+                                  },
+                                  onEditingComplete: () {
+                                    focusNode.unfocus(
+                                        disposition: UnfocusDisposition
+                                            .previouslyFocusedChild);
+                                  },
+                                ),
+                                const SizedBox(height: 12),
+                                const Text(
+                                  'Without FocusNode. This will cause focus to be misplaced.',
+                                  style: TextStyle(color: Colors.white),
+                                  textAlign: TextAlign.center,
+                                ),
+                                const TextField(
+                                  autofocus: false,
+                                ),
+                              ],
                             ),
                           ),
                         ),
